a87aa7469f8705b3da82412b202bcbd80b9cccc7
Ba9u <46553375+Ba9u@users.noreply.github.com>
2019-01-19 16:37:45 +0900

    php

---
+++ up.php
@@ -0,0 +1,20 @@
+﻿<?php
+$tempfile = $_FILES['fname']['tmp_name'];
+$filename = './' . $_FILES['fname']['name'];
+ 
+if (is_uploaded_file($tempfile)) {
+    if ( move_uploaded_file($tempfile , $filename )) {
+	echo $filename . "をアップロードしました。";
+    } else {
+        echo "ファイルをアップロードできません。";
+    }
+} else {
+    echo "ファイルが選択されていません。";
+} 
+$filename=$_FILES['fname']['name'];
+$img = base64_encode(file_get_contents($filename));
+$jsonstr=json_encode($img);
+$fullPath =
+      'python ./cgi-bin/call_from_php.py '.$base64_string;
+    exec($fullPath, $outpara);
+?>
---
+++ upp.php
@@ -0,0 +1,4 @@
+﻿<form action="up.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="fname">
+  <input type="submit" value="アップロード">
+</form>
